368054fb37ef77ed98b74b8a63811654044ebd6a
Ipshita Diwan <24778311+ipshita-diwan@users.noreply.github.com>
2024-05-01 11:53:18 +0530

    Added unit test and integration tests

--- src/main/java/com/polarbookshop/catalogservice/web/BookController.java
+++ src/main/java/com/polarbookshop/catalogservice/web/BookController.java
@@ -14,25 +14,33 @@ public class BookController {
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
+
     @GetMapping
     public Iterable<Book> get() {
         return bookService.viewBookList();
     }
 
+
     @GetMapping("{isbn}")
     public Book getByIsbn(@PathVariable String isbn) {
         return bookService.viewBookDetails(isbn);
     }
+
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book post(@Valid @RequestBody Book book) {
         return bookService.addBookToCatalog(book);
     }
+
+
     @DeleteMapping("{isbn}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String isbn) {
         bookService.removeBookFromCatalog(isbn);
     }
+
+
     @PutMapping("{isbn}")
     public Book put(@PathVariable String isbn, @Valid @RequestBody Book book) {
         return bookService.editBookDetails(isbn, book);
